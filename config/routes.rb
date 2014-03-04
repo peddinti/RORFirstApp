bec5bc725d94147484485ddbc7fabd72115bfc20
Viswa Mani Kiran Peddinti <viswa_manikiran@yahoo.com>
2014-03-04 12:18:09 -0800

    Creating a new Micro Blogging App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FirstApp::Application.routes.draw do
+  resources :microposts
+
+  resources :micro_posts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
