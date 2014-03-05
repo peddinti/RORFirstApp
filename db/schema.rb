27b80e131fcc6d418eb12d1efb2dad37480e69ba
Viswa Mani Kiran Peddinti <viswa_manikiran@yahoo.com>
2014-03-04 16:48:50 -0800

    Finishing Static Pages

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20140304195950) do
 
+  create_table "micro_posts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
